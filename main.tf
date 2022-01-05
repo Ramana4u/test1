931706114a21e2da5328806607e44626c4e8f53a
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-05 21:16:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,5 @@ module "A" {
 }
   module "B" {
     source = "git::https://github.com/Ramana4u/test1.git?ref=main1"
-    security_groups = var.security_groups
-      target_group_arns = [module.A.TG_ARN]
+     target_group_arns = [module.A.TG_ARN]
  }
