ba6ff4e81a5943769f6e326766b0b47c47f885f1
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-06 09:09:29 +0530

    Update moduleasg.tf

--- moduleasg.tf
+++ moduleasg.tf
@@ -5,12 +5,3 @@ resource "aws_launch_configuration" "as_conf" {
   security_groups    = var.security_groups
   user_data = "${file("userdata.sh")}"
 }
-resource "aws_autoscaling_group" "bar" {
-  name                 = var.name 
-  depends_on           = ["aws_launch_configuration.as_conf","aws_lb_target_group.test"]
-  launch_configuration = aws_launch_configuration.as_conf.name
-  min_size             = 1
-  max_size             = 2
-  desired_capacity     = 1
- availability_zones = var.availability_zones
-}
