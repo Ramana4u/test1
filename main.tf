ca1954a756df877aefc0cb598557de8c42d4a630
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-06 09:51:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ module "A" {
     }
   module "B" {
     source = "git::https://github.com/Ramana4u/test1.git?ref=main1"
-    target_group_arns = module.A.target_group_arns
+    target_group_arns = module.A.TG_ARN
  }
