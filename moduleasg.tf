afbcba6a4276545fc332fe5079816c9fec7d5db9
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-06 09:12:36 +0530

    Update moduleasg.tf

--- moduleasg.tf
+++ moduleasg.tf
@@ -1,3 +1,12 @@
+resource "aws_autoscaling_group" "bar" {
+  name                 = var.name 
+  depends_on           = ["aws_launch_configuration.as_conf","aws_lb_target_group.test"]
+  launch_configuration = aws_launch_configuration.as_conf.name
+  min_size             = 1
+  max_size             = 2
+  desired_capacity     = 1
+ availability_zones = var.availability_zones
+}
 resource "aws_launch_configuration" "as_conf" {
   name_prefix   = var.name_prefix
   image_id      = var.image_id
