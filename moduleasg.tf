00340e81c04881c13ab5fc9d6e128d695c64d735
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-06 09:38:49 +0530

    Update moduleasg.tf

--- moduleasg.tf
+++ moduleasg.tf
@@ -1,6 +1,6 @@
 resource "aws_autoscaling_group" "bar" {
   name                 = var.name 
-  depends_on           = ["aws_launch_configuration.as_conf","aws_lb_target_group.test"]
+  depends_on           = ["aws_launch_configuration.as_conf"]
   launch_configuration = aws_launch_configuration.as_conf.name
   min_size             = 1
   max_size             = 2
