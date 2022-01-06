22669daf4f33e25c20d6943c30242c05db7a9559
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-06 09:04:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,11 @@ provider "aws"{
 }
 module "A" {
   source = "git::https://github.com/Ramana4u/test1.git?ref=main2"
+  output "TG_ARN" {
+  value = module.A.TG_ARN
+    }
 }
   module "B" {
     source = "git::https://github.com/Ramana4u/test1.git?ref=main1"
+    target_group_arns = module.A.TG_ARN
  }
