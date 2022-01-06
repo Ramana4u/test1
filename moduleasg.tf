3da09cc5a6a10176fe98217f34d00f7e80efc028
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-06 09:39:39 +0530

    Update moduleasg.tf

--- moduleasg.tf
+++ moduleasg.tf
@@ -1,3 +1,10 @@
+resource "aws_launch_configuration" "as_conf" {
+  name_prefix   = var.name_prefix
+  image_id      = var.image_id
+  instance_type = var.instance_type
+  security_groups    = var.security_groups
+  user_data = "${file("userdata.sh")}"
+}
 resource "aws_autoscaling_group" "bar" {
   name                 = var.name 
   depends_on           = ["aws_launch_configuration.as_conf"]
@@ -6,11 +13,4 @@ resource "aws_autoscaling_group" "bar" {
   max_size             = 2
   desired_capacity     = 1
  availability_zones = var.availability_zones
-}
-resource "aws_launch_configuration" "as_conf" {
-  name_prefix   = var.name_prefix
-  image_id      = var.image_id
-  instance_type = var.instance_type
-  security_groups    = var.security_groups
-  user_data = "${file("userdata.sh")}"
 }
